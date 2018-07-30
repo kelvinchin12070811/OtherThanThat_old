d9527883aa9699aef0c3447f7c6a96bc8d139431
Kelvin Chin <chinkaiwen@yahoo.com>
2018-07-30 09:00:27 +0800

    Added more shortcut keys and reformated about OtherThanThat

--- OtherThanThat/src/OtherThanThat.cpp
+++ OtherThanThat/src/OtherThanThat.cpp
@@ -7,8 +7,10 @@
 OtherThanThat::OtherThanThat(const QUrl& url, bool mainWindow, QWidget *parent)
 	: QMainWindow(parent), url(url), mainWindow(mainWindow)
 {
+	this->setWindowTitle("OtherThanThat - Unofficial Google Drive Client");
 	setupUI();
 	connectSignals();
+	setupShortcuts();
 }
 
 QWebEngineView * OtherThanThat::getViewer()
@@ -30,6 +32,7 @@ void OtherThanThat::closeEvent(QCloseEvent * closeEv)
 
 void OtherThanThat::setupUI()
 {
+	//Pre-init
 	QSize windowSize(0, 0);
 	windowSize.setWidth(800 * Config::getInstance().getZoomFactor());
 	windowSize.setHeight(600 * Config::getInstance().getZoomFactor());
@@ -45,18 +48,65 @@ void OtherThanThat::setupUI()
 		winGeo.close();
 	}
 
+	//Create GUI elements
 	content = new WebPageViewer(this);
+
+	//Create shortcuts
+	helpShortcut = new QShortcut(this);
+	helpShortcut2 = new QShortcut(this);
+	zoomInSc = new QShortcut(this);
+	zoomOutSc = new QShortcut(this);
+	resetZoomSc = new QShortcut(this);
+	refreshSc = new QShortcut(this);
+
+	//GUI initializing
 	content->load(url);
 	
+	//Layout initializing
 	this->setCentralWidget(content);
 }
 
 void OtherThanThat::connectSignals()
 {
+	//Gui elements
 	connect(content, &QWebEngineView::titleChanged, this, &OtherThanThat::on_titleChanger);
+	
+	//Shortcuts
+	connect(helpShortcut, &QShortcut::activated, this, &OtherThanThat::on_helpActionTriggered);
+	connect(helpShortcut2, &QShortcut::activated, [&]() {
+		QMessageBox::about(this, "OtherThanThat, an unofficial Google Drive Client By Kelvin Chin",
+			"<h1>OtherThanThat v1.0</h1>\n\nOtherThanThat is an unofficial Google Drive client by Kelvin Chin");
+	});
+	connect(zoomInSc, &QShortcut::activated, [&]() {
+		this->getViewer()->setZoomFactor(this->getViewer()->zoomFactor() + 0.01f);
+	});
+	connect(zoomOutSc, &QShortcut::activated, [&]() {
+		this->getViewer()->setZoomFactor(this->getViewer()->zoomFactor() - 0.01f);
+	});
+	connect(zoomOutSc, &QShortcut::activated, [&]() {
+		this->getViewer()->setZoomFactor(Config::getInstance().getZoomFactor());
+	});
+	connect(refreshSc, &QShortcut::activated, [&]() {
+		this->getViewer()->reload();
+	});
+}
+
+void OtherThanThat::setupShortcuts()
+{
+	helpShortcut->setKey(Qt::CTRL + Qt::SHIFT + Qt::Key_F12);
+	helpShortcut2->setKey(Qt::CTRL + Qt::Key_F12);
+	zoomInSc->setKey(Qt::CTRL + Qt::Key_Equal);
+	zoomOutSc->setKey(QKeySequence::StandardKey::ZoomOut);
+	resetZoomSc->setKey(Qt::CTRL + Qt::Key_F5);
+	refreshSc->setKey(QKeySequence::StandardKey::Refresh);
+}
+
+void OtherThanThat::on_helpActionTriggered()
+{
+	QMessageBox::aboutQt(this, "OtherThanThat, an unofficial Google Drive Client By Kelvin Chin");
 }
 
 void OtherThanThat::on_titleChanger(const QString& newTitle)
 {
-	this->setWindowTitle(newTitle + " on Other Than That");
+	this->setWindowTitle(newTitle + " on OtherThanThat");
 }
--- OtherThanThat/src/OtherThanThat.hpp
+++ OtherThanThat/src/OtherThanThat.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <qshortcut.h>
 #include <QtWidgets/QMainWindow>
 #include "webpage_viewer/WebPageViewer.hpp"
 
@@ -15,10 +15,19 @@ protected:
 private:
 	void setupUI();
 	void connectSignals();
+	void setupShortcuts();
 private slots:
 	void on_titleChanger(const QString& newTitle);
+	void on_helpActionTriggered();
 private:
 	WebPageViewer * content{ nullptr };
 	QUrl url;
 	bool mainWindow{ false };
+
+	QShortcut* helpShortcut{ nullptr };
+	QShortcut* helpShortcut2{ nullptr };
+	QShortcut* zoomInSc{ nullptr };
+	QShortcut* zoomOutSc{ nullptr };
+	QShortcut* resetZoomSc{ nullptr };
+	QShortcut* refreshSc{ nullptr };
 };
